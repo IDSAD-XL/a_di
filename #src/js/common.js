f11e449fee46ebc3e768736dcde9f4d04c6d01e8
IDSAD <idsad.ch@gmail.com>
2021-03-31 01:35:15 +0200

    desktop header

---
+++ #src/js/common.js
@@ -0,0 +1,15 @@
+$('#headerSlider').slick({
+	arrows: false,
+	dots: true,
+	infinite: true,
+	vertical: true,
+	autoplay: true,
+	autoplaySpeed: 3000,
+	touchMove: false,
+	swipe: false,
+	adaptiveHeight: true,
+	customPaging: function (slider, i) {
+		var thumb = $(slider.$slides[i]).data('thumb');
+		return '<div class="dot"></div>';
+	}
+});
